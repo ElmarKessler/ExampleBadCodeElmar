e663adff666fa542dbe07ae573c8420e0a96bf06
elmar-kessler <elmar.kessler@noseryoung.com>
2022-10-05 16:13:59 +0200

    bad code upload

---
+++ Bad Code App KW40/main.c
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: elmar
+ *
+ * Created on 5 October 2022, 15:48
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Willkommen zum Rechner.\nDieser Rechner kann Zahlen addieren.\nGib zwei beliebige Zahlen ein:/n");
+    float a;
+                                                                                    float b;
+                               int mynum = 5.28 + 8.95;
+                   scanf("%f", &a)       
+                           scanf("%f", &b)  
+                           
+                           float temp;
+                   a = temp;
+                   temp = a;
+                   float result = a+b;
+                   printf("%f", result);
+                                                                    
+                               
+                               
+                               
+                               
+        return (EXIT_SUCCESS);
+}
+
